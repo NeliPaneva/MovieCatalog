70f00604ebdf9089951d96195dc9404241f9fd31
NeliPaneva <ppaneva.neli@gmail.com>
2024-03-02 10:10:21 +0200

    Add project files.

---
+++ Movie.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Movie.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
