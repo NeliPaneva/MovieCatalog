4cc5328b8a204ced23e76256a2436aa86cd1ce0c
NeliPaneva <ppaneva.neli@gmail.com>
2024-03-10 20:24:09 +0200

    Home About

--- Movie.Web/Controllers/HomeController.cs
+++ Movie.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movie.Data;
 using Movie.Web.Models;
 using System.Diagnostics;
 
@@ -7,10 +9,12 @@ namespace Movie.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MovieContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,MovieContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -27,6 +31,19 @@ namespace Movie.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public async Task<ActionResult> About()
+        {
+            IQueryable<MovieGroup> data =
+                from movie in _context.Movies
+                group movie by movie.Company.Name into dateGroup
+                select new MovieGroup()
+                {
+                    CompanyName = dateGroup.Key,
+                    MovieCount = dateGroup.Count()
+                } ;
+            data=data.OrderByDescending(x=>x.MovieCount);
+            return View(await data.AsNoTracking().ToListAsync());
         }
     }
 }
---
+++ Movie.Web/Models/MovieGroup.cs
@@ -0,0 +1,13 @@
+﻿using Movie.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Movie.Web.Models
+{
+    public class MovieGroup
+    {
+        
+        public string CompanyName { get; set; }
+
+        public int MovieCount { get; set; }
+    }
+}
